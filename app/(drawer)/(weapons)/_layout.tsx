a0922271b18487b4683325ace71b20bf462033ce
Shun <ruka4662@gmail.com>
2026-02-19 19:42:27 +0630

    Remove all files

--- app/(drawer)/(weapons)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'expo-router';
-
-export default function WeaponLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="weapon" options={{ headerShown: false }} />
-    </Stack>
-  );
-}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,5 @@
-import { Stack } from 'expo-router';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack>
-        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-      </Stack>
-    </GestureHandlerRootView>
-  );
+  return <Stack />;
 }
