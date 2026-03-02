d9057835483218c6845bf9a79803eaee4bd62567
Shun <ruka4662@gmail.com>
2026-03-02 12:28:04 +0630

    weapon ascension pull to refresh complete (#61)
    ok

--- src/app/resources/materials/(drawer)/weapon/(tabs)/_layout.tsx
+++ src/app/resources/materials/(drawer)/weapon/(tabs)/_layout.tsx
@@ -1,44 +1,47 @@
 import AntDesign from "@expo/vector-icons/AntDesign";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Tabs } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function WeaponTabs() {
 	return (
-		<Tabs
-			safeAreaInsets={{
-				top: 0,
-			}}
-			screenOptions={{
-				tabBarPosition: "top",
-			}}
-		>
-			<Tabs.Screen
-				name="index"
-				options={{
-					href: null,
-					headerShown: false,
+		<SafeAreaProvider>
+			<Tabs
+				safeAreaInsets={{
+					top: 0,
 				}}
-			/>
-			<Tabs.Screen
-				name="ascension/index"
-				options={{
-					title: "Ascension",
-					headerShown: false,
-					tabBarIcon: ({ size, color }) => (
-						<MaterialIcons name="upgrade" size={size} color={color} />
-					),
+				screenOptions={{
+					tabBarPosition: "top",
 				}}
-			/>
-			<Tabs.Screen
-				name="experience/index"
-				options={{
-					title: "Experience",
-					headerShown: false,
-					tabBarIcon: ({ size, color }) => (
-						<AntDesign name="deployment-unit" size={size} color={color} />
-					),
-				}}
-			/>
-		</Tabs>
+			>
+				<Tabs.Screen
+					name="index"
+					options={{
+						href: null,
+						headerShown: false,
+					}}
+				/>
+				<Tabs.Screen
+					name="ascension/index"
+					options={{
+						title: "Ascension",
+						headerShown: false,
+						tabBarIcon: ({ size, color }) => (
+							<MaterialIcons name="upgrade" size={size} color={color} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="experience/index"
+					options={{
+						title: "Experience",
+						headerShown: false,
+						tabBarIcon: ({ size, color }) => (
+							<AntDesign name="deployment-unit" size={size} color={color} />
+						),
+					}}
+				/>
+			</Tabs>
+		</SafeAreaProvider>
 	);
 }
