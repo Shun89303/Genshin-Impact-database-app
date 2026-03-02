028e8a66ec7b46900673cc9b47df7cbc840062b4
Shun <ruka4662@gmail.com>
2026-03-02 10:41:33 +0630

    weapon pull-to-refresh complete (#56)
    ok

--- src/components/weapons/searchList.tsx
+++ src/components/weapons/searchList.tsx
@@ -4,17 +4,23 @@ import WeaponImage from "./weaponImage";
 
 export default function SearchList({
 	finalData,
+	refreshing,
+	onRefresh,
 }: {
 	finalData: Weapon[] | { label: string; data: Weapon[] }[];
+	refreshing: boolean;
+	onRefresh: any;
 }) {
 	return (
 		<View style={{ alignItems: "center" }}>
 			<FlatList
 				data={finalData as Weapon[]}
-				initialNumToRender={6}
+				initialNumToRender={15}
 				windowSize={21}
 				removeClippedSubviews
 				numColumns={3}
+				refreshing={refreshing}
+				onRefresh={onRefresh}
 				renderItem={({ item }) => (
 					<View style={{ justifyContent: "space-evenly", padding: 10 }}>
 						<WeaponImage id={item.id} />
