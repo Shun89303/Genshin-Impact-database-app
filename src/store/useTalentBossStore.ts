77e197b1c37933829e134da29782570601eaaf13
Shun <ruka4662@gmail.com>
2026-03-03 15:42:34 +0630

    talent boss pre-filter complete (#74)
    ok

--- src/store/useTalentBossStore.ts
+++ src/store/useTalentBossStore.ts
@@ -3,21 +3,18 @@ import {
 	getAllTalentBossMaterialImageIds,
 	getAllTalentBossMaterialsData,
 } from "../services/talent.boss.materials.services";
-import { TalentBoss } from "../types/talent.boss";
+import { ApiObject } from "../types/talent.boss";
 
 interface TalentBossMaterialsState {
 	error: string | null;
 	input: string;
-	details: TalentBoss[];
+	details: ApiObject[];
 	cache: Record<string, unknown>;
 	loadingId: string | null;
-	materialsObject: Record<string, unknown>;
 	materialIds: string[];
 	setInput: (i: string) => void;
 	fetchAllDetails: () => Promise<void>;
-	fetchMaterialsObject: () => Promise<void>;
 	fetchMaterialIds: () => Promise<void>;
-	storeMaterialDetails: (id: string) => void;
 }
 
 export const useTalentBossMaterialsStore = create<TalentBossMaterialsState>(
@@ -30,19 +27,6 @@ export const useTalentBossMaterialsStore = create<TalentBossMaterialsState>(
 		loadingId: null,
 		materialsObject: {},
 		materialIds: [],
-		fetchMaterialsObject: async () => {
-			const { materialsObject } = get();
-			if (Object.keys(materialsObject).length > 0) return;
-			try {
-				const allMaterials = await getAllTalentBossMaterialsData();
-				const allMaterialsArray = Object.entries(allMaterials);
-				const withoutListItem = allMaterialsArray.slice(0, -1);
-				const materialsObject = Object.fromEntries(withoutListItem);
-				set({ materialsObject });
-			} catch (error: any) {
-				set({ error: error.message });
-			}
-		},
 		fetchMaterialIds: async () => {
 			try {
 				const { materialIds } = get();
@@ -55,28 +39,17 @@ export const useTalentBossMaterialsStore = create<TalentBossMaterialsState>(
 				set({ error: error.message });
 			}
 		},
-		storeMaterialDetails: (id) => {
-			const { materialsObject, cache } = get();
-			if (cache[id]) {
-				set({ loadingId: null });
-				return;
-			}
-
-			set({ loadingId: id });
-			const details = materialsObject[id];
-			set((state) => ({
-				cache: { ...state.cache, [id]: details },
-				loadingId: null,
-			}));
-		},
 		fetchAllDetails: async () => {
 			try {
 				let { details } = get();
 				if (!details.length) {
 					const apiObject = await getAllTalentBossMaterialsData();
 					const { id, ...cleanedObject } = apiObject;
-					const talentBossArray: TalentBoss[] = Object.values(cleanedObject);
-					set({ details: talentBossArray });
+					const cleanedArray = Object.entries(cleanedObject);
+					const normalizedArray = cleanedArray.map(([key, value]) => ({
+						...value,
+					}));
+					set({ details: normalizedArray });
 				}
 			} catch (error: any) {
 				set({ error: error.message });
